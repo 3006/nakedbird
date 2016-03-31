4c162ae79f5abd29422362934c5fdf4ef9a601b3
Chao Song <songchao43088@gmail.com>
2016-03-31 01:27:16 -0700

    Added feature to flip screen.

--- bin/launch_preview.py
+++ bin/launch_preview.py
@@ -2,9 +2,18 @@
 
 import picamera
 import time
+import datetime
 
 camera=picamera.PiCamera()
+camera.resolution=(1920, 1080)
 print "hellowworld"
-camera.start_preview()
+camera.start_preview(fullscreen=False, window=(100,100,1000,1000))
 while True:
-    pass
+	command = raw_input("")
+	if command.rstrip() == "a":
+		camera.hflip = not camera.hflip
+	elif command.rstrip() == "q":
+		camera.vflip = not camera.vflip
+	elif command.rstrip() == "":
+		name = "/home/pi/picture/%s.jpg" % datetime.datetime.now().strftime("%y-%m-%d-%H-%M-%s")
+		camera.capture(name)
